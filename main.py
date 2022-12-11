6f359a174b4698e01fbec4fb6e070a39df030b2e
Simone Silvetti <simone.silvetti@gmail.com>
2022-12-11 15:29:11 +0000

    [gcp cloud] improvement

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from flask import jsonify
 
 app = Flask(__name__)
 
-#app.route('/')
+@app.route('/')
 def hello():
-    return "Hello"
+    return "Hello ciccio!"
 
-@app.route('/name/<name>')
+@app.route('/name/<value>')
 def name(value):
     return jsonify({"value":value})
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port:8080, debug=True)
+    app.run(host='127.0.0.1', port=8080, debug=True)
